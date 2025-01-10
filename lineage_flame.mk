cde5afd4e169de636e789fa9d78f577230df5f9c
Khaliq <61682259+khaliq8145@users.noreply.github.com>
2025-01-10 14:28:04 +0700

    Flame: update rising build flags

--- lineage_flame.mk
+++ lineage_flame.mk
@@ -20,10 +20,22 @@ PRODUCT_NAME := lineage_flame
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Rising flags
+RISING_PACKAGE_TYPE := GAPPS
+RISING_MAINTAINER="Bias Khaliq (Morpheus)"
+TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := true
+
+# GMS build flags
+WITH_GMS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="flame-user 13 TP1A.221005.002.B2 9382335 release-keys" \
     BuildFingerprint=google/flame/flame:13/TP1A.221005.002.B2/9382335:user/release-keys \
-    DeviceProduct=flame
+    DeviceProduct=flame \
+    RisingChipset="Snapdragon 855" \
+    RisingMaintainer="Bias Khaliq"
 
 $(call inherit-product, vendor/google/flame/flame-vendor.mk)
